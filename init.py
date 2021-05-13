a3e2419bca8048e993ecad4cae5542e98ee3057b
Wes <wrbigsky@gmail.com>
2021-05-13 00:28:30 -0600

    debug

--- init.py
+++ init.py
@@ -8,6 +8,7 @@ import database
 """ config files copied from drive """
 
 if not current_settings.local:
+    print("HERE\n\n\n\n\nHERE")
     os.mkdir('/root/.aws/')
     creds = '/content/drive/MyDrive/feature-attribution/config/'+current_settings.name+'-credentials'
     shutil.copy(creds, '/root/.aws/credentials')
