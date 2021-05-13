6ee6e84dddd3da01882ac1101c693194b1c48752
Wes <wrbigsky@gmail.com>
2021-05-13 12:47:32 -0600

    dataset

--- datasets.py
+++ datasets.py
@@ -18,14 +18,14 @@ data = {}
 
 batch_size = 64
 if 'cifar' in current_settings.datasets:
-    data['cifar_trs'] = datasets.CIFAR10(root='./data/cifar', train=True, download=True, transform=transform)
-    data['cifar_trl'] = DataLoader(cifar_trs, batch_size=64,shuffle=True, num_workers=2)
-    data['cifar_ts'] = datasets.CIFAR10(root='./data/cifar', train=False, download=True, transform=transform)
-    data['cifar_tl'] = DataLoader(cifar_ts, batch_size=64, shuffle=False, num_workers=2)
+    data['cifar_trs'] = datasets.CIFAR10(root='/content/data/cifar', train=True, download=True, transform=transform)
+    data['cifar_trl'] = DataLoader(data['cifar_trs'], batch_size=64,shuffle=True, num_workers=2)
+    data['cifar_ts'] = datasets.CIFAR10(root='/content/data/cifar', train=False, download=True, transform=transform)
+    data['cifar_tl'] = DataLoader(data['cifar_ts'], batch_size=64, shuffle=False, num_workers=2)
     cifar_classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
 if 'mnist' in current_settings.datasets:
-    data['mnist_trs'] = datasets.MNIST(root='./data/mnist', train=True, transform=transforms.ToTensor(), download=True)
-    data['mnist_trl'] = DataLoader(dataset=mnist_trs, batch_size=batch_size, shuffle=True) # shuffle = true shuffles every epoch so new bacthes are made
-    data['mnist_ts'] = datasets.MNIST(root='./data/mnist', train=False, transform=transforms.ToTensor(), download=True)
-    data['mnist_tl'] = DataLoader(dataset=mnist_ts, batch_size=batch_size, shuffle=True)
+    data['mnist_trs'] = datasets.MNIST(root='/content/data/mnist', train=True, transform=transforms.ToTensor(), download=True)
+    data['mnist_trl'] = DataLoader(dataset=data['mnist_trs'], batch_size=batch_size, shuffle=True) # shuffle = true shuffles every epoch so new bacthes are made
+    data['mnist_ts'] = datasets.MNIST(root='/content/data/mnist', train=False, transform=transforms.ToTensor(), download=True)
+    data['mnist_tl'] = DataLoader(dataset=data['mnist_ts'], batch_size=batch_size, shuffle=True)
