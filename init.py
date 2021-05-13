4ada9c3c76bed1e4bcd1d7dafb801980e8847549
Wes <wrbigsky@gmail.com>
2021-05-13 00:22:02 -0600

    file copy fix

--- init.py
+++ init.py
@@ -10,7 +10,7 @@ import database
 if not current_settings.local:
     creds = '/content/drive/MyDrive/feature-attribution/config/'+current_settings.name+'-credentials'
     shutil.copy(creds, '/root/.aws/credentials')
-    shutil.copy(creds ,'/root/.aws/credentials')
+    shutil.copy('/content/drive/MyDrive/feature-attribution/config/config','/root/.aws/')
 
 """ global vars """
 database = database.Database()
