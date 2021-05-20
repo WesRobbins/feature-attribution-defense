9cdf883aefa4e07e456462b3c2785c5c88b4ee1a
Wes <wrbigsky@gmail.com>
2021-05-20 16:56:09 -0600

    updates

--- config/config.py
+++ config/config.py
@@ -7,6 +7,7 @@ import shutil
 local = False
 user = 'wes'
 accepted = ['-local', 'config/config.py','config.py', '-seth', '-wes']
+
 for i in sys.argv:
     if i not in accepted:
         print(f'Error: \"{i}\" is not an accpeted arguement')
