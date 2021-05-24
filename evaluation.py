7ab51b00999ed79b2a811af6db960810525e60c6
Wes <wrbigsky@gmail.com>
2021-05-23 19:08:51 -0600

    typo

--- evaluation.py
+++ evaluation.py
@@ -5,6 +5,7 @@ import torch.nn.functional as F
 import torch.backends.cudnn as cudnn
 
 from settings import current_settings
+from dataset import data
 
 import progressbar
 widgets = [progressbar.Percentage(), progressbar.Bar(), progressbar.ETA()]
