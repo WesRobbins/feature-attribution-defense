f72f1990c63573eca305371abf219d84d0ad11e4
Wes <wrbigsky@gmail.com>
2021-05-30 22:10:36 -0600

    renaming

--- evaluation.py
+++ evaluation.py
@@ -16,28 +16,28 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 widgets = [progressbar.Percentage(), progressbar.Bar(), progressbar.ETA()]
 
-def full_run(db, attack_names, models, attack_strenghts, defense=None, dataset='cifar'):
+def full_run(db, atk_names, models, attack_strenghts, defense=None, dataset='cifar'):
     for atck in att:
         for model in models:
-            for stren in attack_strengths:
+            for stren in eps:
                 eval_and_write(db, model, atck, defense, stren, dataset)
 
-def eval_and_write(db, net_name, attack=None, defense=None, attack_strength=None, dataset='cifar'):
+def eval_and_write(db, net_name, atk=None, defense=None, eps=None, dataset='cifar'):
 
-    results = evaluation_loop(net_name, dataset, attack=attack, defense=defense,
-                              attack_strength=attack_strength)
-    if not attack:
-      attack = 'none'
+    results = evaluation_loop(net_name, dataset, atk=atk, defense=defense,
+                              eps=eps)
+    if not atk:
+      atk = 'none'
     if not defense:
       defense = 'none'
-    if not attack_strength:
-      attack_strength = 'n/a'
+    if not eps:
+      eps = 'n/a'
 
-    db.write(attack=attack, defense=defense, attack_strength=attack_strength,
+    db.write(atk=atk, defense=defense, eps=eps,
                           model=net_name, dataset=dataset, accuracy=results[0], loss=results[1], l2=results[2])
 
 # returns loss, accuracy
-def evaluation_loop(net_name, dataset, epoch=None, attack=None, defense=None, attack_strength=None):
+def evaluation_loop(net_name, dataset, epoch=None, atk=None, defense=None, eps=None):
     net = current_settings.loaded_models[dataset][net_name]
     ds_name = dataset + '_tl'
     testloader = data[ds_name]
@@ -48,10 +48,10 @@ def evaluation_loop(net_name, dataset, epoch=None, attack=None, defense=None, at
     correct = 0
     total = 0
     l2 = 'n/a'
-    if attack:
+    if atk:
       # cont = nullcontext()
       l2 = 0
-      atck = get_attack(net, attack, attack_strength)
+      atck = get_atk(net, atk, eps)
     else:
       # cont = torch.no_grad()
       pass
@@ -61,11 +61,11 @@ def evaluation_loop(net_name, dataset, epoch=None, attack=None, defense=None, at
 
 
     # with cont:
-    if attack: bar = progressbar.ProgressBar(max_value=len(testloader), widgets=widgets)
+    if atk: bar = progressbar.ProgressBar(max_value=len(testloader), widgets=widgets)
     for batch_idx, (inputs, targets) in enumerate(testloader):
         inputs, targets = inputs.to(device), targets.to(device)
 
-        if attack:
+        if atk:
           atck_inputs = atck(inputs, targets)
           l2+= mse(inputs, atck_inputs).item()
           inputs = atck_inputs
@@ -75,16 +75,16 @@ def evaluation_loop(net_name, dataset, epoch=None, attack=None, defense=None, at
         _, predicted = outputs.max(1)
         total += targets.size(0)
         correct += predicted.eq(targets).sum().item()
-        if attack: bar.update(batch_idx)
+        if atk: bar.update(batch_idx)
 
-    if attack:
+    if atk:
       bar.finish()
-      print(f'{attack} attack')
+      print(f'{atk} atk')
       l2 = l2/len(testloader)
 
     loss = test_loss/len(testloader)
     acc = 100.*correct/total
-    print(f'Model: {net_name}   Attack: {attack}    Defense: {defense}  Dataset: {dataset}')
+    print(f'Model: {net_name}   Attack: {atk}    Defense: {defense}  Dataset: {dataset}')
     print(f'METRICS - Loss: {loss:.3f} | Acc: {acc:.3f} {correct}/{total} | l2: {l2}')
 
 
@@ -92,48 +92,43 @@ def evaluation_loop(net_name, dataset, epoch=None, attack=None, defense=None, at
     return acc, loss, l2
 
 
-def get_attack(model, attack_name, attack_strength):
-    if 'eps' in attack_strength.keys():
-        eps = float(attack_strength['eps'])
-    else:
-        eps = None
-    if 'steps' in attack_strength.keys():
-        steps = attack_strength['steps']
-    else:
-        steps = None
-    if attack_name == 'fgsm':
+def get_atk(model, atk_name, eps):
+
+    eps = float(eps['eps'])
+
+    if atk_name == 'fgsm':
         return torchattacks.FGSM(model, eps=eps)
-    elif attack_name == 'bim':
+    elif atk_name == 'bim':
         return torchattacks.BIM(model, eps=eps, steps=steps, alpha=eps/(steps*.5))
-    elif attack_name == 'deepfool':
+    elif atk_name == 'deepfool':
         return torchattacks.DeepFool(model, steps=steps)
-    elif attack_name == 'cw':
+    elif atk_name == 'cw':
         return torchattacks.CW(model)
-    elif attack_name == 'pgd':
+    elif atk_name == 'pgd':
         return torchattacks.PGD(model, eps=eps, steps=steps, alpha=eps/(steps*.5))
-    elif attack_name == 'rfgsm':
+    elif atk_name == 'rfgsm':
         return torchattacks.RFGSM(model, eps=eps, alpha=eps)
-    elif attack_name == 'auto-attack':
+    elif atk_name == 'auto-attack':
         return torchattacks.AutoAttack(model, eps=eps)
-    elif attack_name == 'mifgsm':
+    elif atk_name == 'mifgsm':
         return torchattacks.MIFGSM(model, eps=eps, steps=steps)
-    elif attack_name == 'square':
+    elif atk_name == 'square':
         return torchattacks.Square(model, eps=eps)
-    elif attack_name == 'fab':
+    elif atk_name == 'fab':
         return torchattacks.FAB(model, eps=eps)
-    elif attack_name == 'one-pixel':
+    elif atk_name == 'one-pixel':
         return torchattacks.OnePixel(model)
-    elif attack_name == 'gn':
+    elif atk_name == 'gn':
         return torchattacks.GN(model, sigma=eps)
-    elif attack_name == 'apgd':
+    elif atk_name == 'apgd':
         return torchattacks.APGD(model, eps=eps)
-    elif attack_name == 'eotpgd':
+    elif atk_name == 'eotpgd':
         return torchattacks.EOTPGD(model, eps=eps, steps=steps, alpha=eps/(steps*.5))
-    elif attack_name == 'pgddlr':
+    elif atk_name == 'pgddlr':
         return torchattacks.PGDDLR(model, eps=eps, steps=steps, alpha=eps/(steps*.5))
-    elif attack_name == 'ffgsm':
+    elif atk_name == 'ffgsm':
         return torchattacks.FFGSM(model, eps=eps, alpha=eps)
-    elif attack_name == 'sparsefool':
+    elif atk_name == 'sparsefool':
         return torchattacks.SparseFool(model)
 
     else:
--- run.py
+++
@@ -1,4 +0,0 @@
-# run experiement loops
-
-def evaluation_loop(model, dataset, attack=None, defense=None):
-    pass
